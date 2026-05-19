b7ced35360bea70ca0805c3af05b485cbce8d9eb
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-19 11:00:12 +0500

    Update AllRooms.jsx

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets, facilityIcons, roomsDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import StarRating from "../components/StarRating";
 
 const AllRooms = () => {
   const navigate = useNavigate();
+  const [openFilters, setOpenFilters] = useState(false);
   return (
     <div
       className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28
@@ -20,7 +21,10 @@ const AllRooms = () => {
         </div>
 
         {roomsDummyData.map((room) => (
-          <div key={room._id} className="flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0">
+          <div
+            key={room._id}
+            className="flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0"
+          >
             <img
               onClick={() => {
                 navigate(`/rooms/${room._id}`);
@@ -52,22 +56,54 @@ const AllRooms = () => {
               </div>
               {/* Room Amentities */}
               <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">
-                {room.amenities.map((item, index)=>(
-                    <div key={index} className="flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70">
-                        <img src={facilityIcons[item]} alt={item} 
-                        className="w-5 h-5"
-                        />
-                        <p className="text-xs">{item}</p>
-                    </div>
+                {room.amenities.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70"
+                  >
+                    <img
+                      src={facilityIcons[item]}
+                      alt={item}
+                      className="w-5 h-5"
+                    />
+                    <p className="text-xs">{item}</p>
+                  </div>
                 ))}
               </div>
               {/* Room Price per Night */}
-              <p className="text-xl font-medium text-gray-700">${room.pricePerNight} /night</p>
+              <p className="text-xl font-medium text-gray-700">
+                ${room.pricePerNight} /night
+              </p>
             </div>
           </div>
         ))}
       </div>
       {/* {Filters} */}
+      <div className="bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 min-lg:mt-16">
+        <div
+          className={`flex items-center justify-between px-5 py-2.5 min-lg:border-b border-gray-300 ${openFilters && "border-b"}`}
+        >
+          <p className="text-base font-medium text-gray-800">FILTERS</p>
+          <div className="text-xs cursor-pointer">
+            <span
+              onClick={() => setOpenFilters(!openFilters)}
+              className="lg:hidden"
+            >
+              {" "}
+              {openFilters ? "HIDE" : "SHOW"}
+            </span>
+            <span className="hidden lg:block">CLEAR</span>
+          </div>
+        </div>
+
+        <div
+          className={`${openFilters ? "h-auto" : "h-0 lg:h-auto"} overflow-hidden transition-all duration-700`}
+        >
+          <div className="px-5 pt-5">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
