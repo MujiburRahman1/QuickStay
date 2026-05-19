6142f8622afa5cb6a7428b227c621a10e1f9853a
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-19 07:14:05 +0500

    Update NewsLetter.jsx

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import Title from "./Title";
 
 const NewsLetter = () => {
   return (
     <div className="flex flex-col items-center max-w-5xl lg:w-full rounded-2xl px-4 py-12 md:py-16 mx-2 lg:mx-auto my-30 bg-gray-900 text-white">
-      
+      <Title
+        title="Stay Inspired"
+        subTitle="Join our newsletter and be the first to discover new destinations, exclusive offers, and travel inspirations."
+      />
+
       <div className="flex flex-col md:flex-row items-center justify-center gap-4 mt-6">
         <input
           type="text"
@@ -13,9 +18,11 @@ const NewsLetter = () => {
         />
         <button className="flex items-center justify-center gap-2 group bg-black px-4 md:px-7 py-2.5 rounded active:scale-95 transition-all">
           Subscribe
-          
-          <img src={assets.arrowIcon} alt="arrow-icon" 
-          className="w-3.5 invert group-hover:translate-x-1 transition-all" />
+          <img
+            src={assets.arrowIcon}
+            alt="arrow-icon"
+            className="w-3.5 invert group-hover:translate-x-1 transition-all"
+          />
         </button>
       </div>
       <p className="text-gray-500 mt-6 text-xs text-center">
