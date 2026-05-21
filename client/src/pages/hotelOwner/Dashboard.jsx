c3eb27e7a2257e40d5bd1369974620cd0803be1b
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-21 19:14:43 +0500

    Update Dashboard.jsx

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Title from '../../components/Title'
+import { assets } from '../../assets/assets'
 
 const Dashboard = () => {
   return (
     <div>
-      
+      <Title align='left' font='outfit' title='Dashboard'
+      subTitle='Monitor your room listings, track bookings and analyze revenue-all in one place. Stay updated with real-time insights to ensure smooth operations.'/>
+        <div className='flex gap-4 my-8'>
+            {/* Total Bookings */}
+            <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+                <img src={assets.totalBookingIcon} alt="" className='max-sm:hidden h-10' />
+
+            </div>
+            {/* Total Revenue */}
+            <div></div>
+
+        </div>
     </div>
   )
 }
