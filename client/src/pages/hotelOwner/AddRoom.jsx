787d10a45ccecde4efc3c199f276295350b4da92
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-22 07:57:38 +0500

    Update AddRoom.jsx

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Form } from "react-router-dom";
+import Title from "../../components/Title";
 
 const AddRoom = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  });
 
-export default AddRoom
+  const [inputs, setInputs] = useState({
+    roomType: "",
+    pricePerNight: 0,
+    amenities: {
+      "Free Wifi": false,
+      "Free Breakfast": false,
+      "Room Service": false,
+      "Mountain View": false,
+      "Pool Access": false,
+    },
+  });
+
+  return (
+    <form>
+      <Title
+        align="left"
+        font="outfit"
+        title="Add Room"
+        subTitle="Fill in the details carefully and accurate room details, pricing and amenities, to enhance the user booking experience."
+      />
+    </form>
+  );
+};
+
+export default AddRoom;
