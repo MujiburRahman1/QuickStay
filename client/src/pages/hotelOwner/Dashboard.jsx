7ee2ab612f2e573d1ade0ba7800fba459936d4d7
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-22 05:29:26 +0500

    Update Dashboard.jsx

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../../components/Title'
-import { assets } from '../../assets/assets'
+import { assets, dashboardDummyData } from '../../assets/assets'
 
 const Dashboard = () => {
+
+  const [dashboardData, setDashboardData] = useState(dashboardDummyData)
+
   return (
     <div>
       <Title align='left' font='outfit' title='Dashboard'
@@ -11,10 +14,21 @@ const Dashboard = () => {
             {/* Total Bookings */}
             <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
                 <img src={assets.totalBookingIcon} alt="" className='max-sm:hidden h-10' />
+                <div className='flex flex-col sm:ml-4 font-medium'>
+                  <p className='text-blue-500 text-lg'>Total Bookings</p>
+                  <p className='text-neutral-400 text-base'>{dashboardData.totalBookings}</p>
+                </div>
 
             </div>
             {/* Total Revenue */}
-            <div></div>
+            <div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+                <img src={assets.totalRevenueIcon} alt="" className='max-sm:hidden h-10' />
+                <div className='flex flex-col sm:ml-4 font-medium'>
+                  <p className='text-blue-500 text-lg'>Total Revenue</p>
+                  <p className='text-neutral-400 text-base'>$ {dashboardData.totalRevenue}</p>
+                </div>
+
+            </div>
 
         </div>
     </div>
