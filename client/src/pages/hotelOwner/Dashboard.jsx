178dd24e31a6ca70066efe3ba60efec3dca3d19a
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-22 05:38:54 +0500

    Update Dashboard.jsx

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -31,6 +31,23 @@ const Dashboard = () => {
             </div>
 
         </div>
+
+        {/* Recent Bookings */}
+        <h2 className='text-xl text-blue-950/70 font-medium mb-5'>Recent Bookings</h2>
+        <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll'>
+          <table className='w-full'>
+            <thead className='bg-gray-50'>
+              <tr>
+                <th className='py-3 px-4 text-gray-800 font-medium'>User Name</th>
+                <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Room Name</th>
+                <th className='py-3 px-4 text-gray-800 font-medium text-center'>Total Amount</th>
+                <th className='py-3 px-4 text-gray-800 font-medium text-center'>Payment Status</th>
+              </tr>
+
+            </thead>
+          </table>
+
+        </div>
     </div>
   )
 }
