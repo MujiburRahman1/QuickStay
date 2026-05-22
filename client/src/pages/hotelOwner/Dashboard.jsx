62b9c3616385bd4343ce1262cf794bdebf45cb97
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-22 07:46:56 +0500

    Update Dashboard.jsx

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -58,11 +58,13 @@ const Dashboard = () => {
                   </td>
 
                   <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
-                    $ {item.user.username}
+                    $ {item.totalPrice}
                   </td>
 
                   <td className='py-3 px-4 border-t border-gray-300 flex'>
-                    <button className={`py-1 px-3 text-xs rounded-full mx-auto ${}`}>
+                    <button className={`py-1 px-3 text-xs rounded-full mx-auto ${item.isPaid ? 'bg-green-200 text-green-600' : 
+                      'bg-amber-200 text-yellow-600'
+                    }`}>
                         {item.isPaid ? 'Completed' : 'Pending'}
                     </button>
                   </td>
