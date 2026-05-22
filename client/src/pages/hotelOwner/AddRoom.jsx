702eb53ab1b10efd2eec580f028405c253a5e55a
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-22 08:59:52 +0500

    Update AddRoom.jsx

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -74,11 +74,14 @@ const AddRoom = () => {
           <div key={index}>
             <input type="checkbox" id={`amenities${index+1}`} checked={inputs.amenities[amenity]}
             onChange={()=>setInputs({...inputs, amenities: {...inputs.amenities, [amenity]: !inputs.amenities[amenity]}})} />
-            <label htmlFor={`amenities${index+1}`}>{amenity}</label>
+            <label htmlFor={`amenities${index+1}`}> {amenity}</label>
           </div>
         ))}
 
       </div>
+      <button className="bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer">
+        Add Room
+      </button>
     </form>
   );
 };
