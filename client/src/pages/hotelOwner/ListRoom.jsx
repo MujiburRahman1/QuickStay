91e7abc1f6ce9e8454da886f0c922e0150984ba0
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-22 09:23:05 +0500

    Update ListRoom.jsx

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -10,6 +10,39 @@ const ListRoom = () => {
     <div>
       <Title align='left' font='outfit' title='Room Listings'
       subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.'/>
+      <p className='text-gray-500 mt-8'>All Rooms</p>
+      <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll mt-3'>
+        <table className='w-full'>
+          <thead className='bg-gray-50'>
+              <tr>
+                <th className='py-3 px-4 text-gray-800 font-medium'>Name</th>
+                <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Facility</th>
+                <th className='py-3 px-4 text-gray-800 font-medium'>Price / night</th>
+                <th className='py-3 px-4 text-gray-800 font-medium text-center'>Actions</th>
+              </tr>
+
+            </thead>
+            <tbody className='text-sm'>
+              {
+                rooms.map((item, index)=>(
+                  <tr key={index}>
+                    <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                      {item.roomType}
+                    </td>
+                    <td className='py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden'>
+                      {item.amenities.join(', ')}
+                    </td>
+                    <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
+                      {item.roomType}
+                    </td>
+
+                  </tr>
+                ))
+              }
+
+            </tbody>
+        </table>
+      </div>
     </div>
   )
 }
