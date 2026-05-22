6e081f3db3473fc799c5f8735e7e551e25944f41
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-22 09:07:06 +0500

    Update ListRoom.jsx

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { roomsDummyData } from '../../assets/assets'
+import Title from '../../components/Title'
 
 const ListRoom = () => {
+
+    const [rooms, setRooms] = useState(roomsDummyData)
+
   return (
     <div>
-      
+      <Title align='left' font='outfit' title='Room Listings'
+      subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.'/>
     </div>
   )
 }
