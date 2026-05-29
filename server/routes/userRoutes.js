60cc8da625f6def6e51e03ca3877e0d97a2483e0
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-29 15:32:50 +0500

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { getUserData } from "../controllers/userController.js";
+import { getUserData, storeRecentSearchedCities } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 
 userRouter.get('/', protect, getUserData);
-userRouter.post('/', protect, getUserData);
+userRouter.post('/store-recent-search', protect, storeRecentSearchedCities);
 
 
 
