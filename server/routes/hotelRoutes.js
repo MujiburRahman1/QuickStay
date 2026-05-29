0db1f3f8cd32a3b7328ecc1134e8190a40089720
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-29 15:47:43 +0500

    Create hotelRoutes.js

---
+++ server/routes/hotelRoutes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { registerHotel } from "../controllers/hotelController"
+import { protect } from "../middleware/authMiddleware"
+
+const hotelRouter = express.Router()
+
+hotelRouter.post('/', protect, registerHotel);
+
+
+
+export default hotelRouter;
