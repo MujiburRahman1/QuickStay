bb614af642986dada54af31c34b0996393018a9f
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-05-29 15:11:34 +0500

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -7,6 +7,8 @@ export const protect = async (res, res, next) => {
   if (!userId) {
     res.json({ success: false, message: "not authenticated" });
   }else{
-    
+    const user = await User.findById(userId);
+    req.user = user;
+    next()
   }
 };
