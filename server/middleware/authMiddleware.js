474edccf0cbbfbab7869d4410145f99655638f01
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-12 09:13:26 +0500

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,13 +2,13 @@ import User from "../models/User.js";
 
 // Middleware to check if user is authenticated
 
-export const protect = async (res, res, next) => {
+export const protect = async (req, res, next) => {
   const { userId } = req.auth;
   if (!userId) {
-    res.json({ success: false, message: "not authenticated" });
-  }else{
-    const user = await User.findById(userId);
-    req.user = user;
-    next()
+    return res.json({ success: false, message: "not authenticated" });
   }
+
+  const user = await User.findById(userId);
+  req.user = user;
+  next();
 };
