9ab5fb97f0985d29ade5ee3dddd545f1213a306f
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-12 08:24:01 +0500

    Update bookingRoutes.js

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { checkAvailabilityAPI } from "../controllers/bookingController.js";
+import { checkAvailabilityAPI, creatBooking, getHotelBookings, getUserBookings } from "../controllers/bookingController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 
 const bookingRouter = express.Router();
 
 bookingRouter.post('/check-availability', checkAvailabilityAPI);
-bookingRouter.post('/book', protect)
+bookingRouter.post('/book', protect, creatBooking);
+bookingRouter.get('/user', protect, getUserBookings);
+bookingRouter.get('/hotel', protect, getHotelBookings);
+
+
+export default bookingRouter;
+
