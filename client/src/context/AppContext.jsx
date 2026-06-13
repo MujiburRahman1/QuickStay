3fbd4b51abc802053b643d6a7616e3cc11dc99fa
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-13 05:46:00 +0500

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,3 +1,45 @@
 import axios from "axios";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser, useAuth } from "@clerk/clerk-react";
 
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const currency = import.meta.env.VITE_CURRENCY || "$";
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { getToken } = useAuth();
+
+  const [isOwner, setIsOwner] = useState(false);
+  const [showHotelReg, setShowHotelReg] = useState(false);
+
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/user", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+      if (data.success) {
+        setIsOwner(data.role === "hotelOwner");
+      }
+    } catch (error) {}
+  };
+
+  const value = {
+    currency,
+    navigate,
+    user,
+    getToken,
+    isOwner,
+    setIsOwner,
+    axios,
+    showHotelReg,
+    setShowHotelReg,
+  };
+
+  return <AppContext value={value}>{children}</AppContext>;
+};
+
+export const useAppContext = () => useContext(AppContext);
