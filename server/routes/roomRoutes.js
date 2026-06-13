6846cab54327b653f2b08afddbb033f1601cf575
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-13 17:01:52 +0500

    Update roomRoutes.js

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -8,7 +8,7 @@ const roomRouter = express.Router();
 
 
 roomRouter.post('/', upload.array("images", 4), protect, creatRoom)
-roomRouter.get('/', getOwnerRooms)
+roomRouter.get('/', getRooms)
 roomRouter.get('/owner', protect, getOwnerRooms)
 roomRouter.post('/toggle-availability', protect, toggleRoomAvailability)
 
