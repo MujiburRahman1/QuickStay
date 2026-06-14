aba981876aed8e7f5b5cb4cffc39acc3685800a5
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-14 14:01:15 +0500

    Update Dashboard.jsx

--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -51,7 +51,7 @@ const Dashboard = () => {
                 <img src={assets.totalRevenueIcon} alt="" className='max-sm:hidden h-10' />
                 <div className='flex flex-col sm:ml-4 font-medium'>
                   <p className='text-blue-500 text-lg'>Total Revenue</p>
-                  <p className='text-neutral-400 text-base'>$ {dashboardData.totalRevenue}</p>
+                  <p className='text-neutral-400 text-base'>{currency} {dashboardData.totalRevenue}</p>
                 </div>
 
             </div>
@@ -84,7 +84,7 @@ const Dashboard = () => {
                   </td>
 
                   <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'>
-                    $ {item.totalPrice}
+                    {currency} {item.totalPrice}
                   </td>
 
                   <td className='py-3 px-4 border-t border-gray-300 flex'>
