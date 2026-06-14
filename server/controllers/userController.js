310ca883a56d448975e6e5547ccd7cbc4d0696ba
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-14 17:31:52 +0500

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,21 +14,25 @@ export const getUserData = async (req, res) =>{
 
 // Store User Recent Searched Cities
 
-export const storeRecentSearchedCities = async (req, res)=>{
- try {
-    const {recentSearchedCities} = req.body
-    const user = await req.user;
+export const storeRecentSearchedCities = async (req, res) => {
+  try {
+    const { recentSearchedCity } = req.body;
+    const user = req.user;
 
-    if(user.recentSearchedCities.lenght < 3){
-        user.recentSearchedCities.push(recentSearchedCities)
-    }else{
-        user.recentSearchedCities.shift();
-        user.recentSearchedCities.push(recentSearchedCities)
+    if (!recentSearchedCity) {
+      return res.json({ success: false, message: "City is required" });
     }
-    await user.save();
-    res.json({success: true, message: "City added"})
 
- } catch (error) {
-    res.json({success: false, message: "error.message"})
- }
-}
+    if (user.recentSearchedCities.length < 3) {
+      user.recentSearchedCities.push(recentSearchedCity);
+    } else {
+      user.recentSearchedCities.shift();
+      user.recentSearchedCities.push(recentSearchedCity);
+    }
+
+    await user.save();
+    res.json({ success: true, message: "City added" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
