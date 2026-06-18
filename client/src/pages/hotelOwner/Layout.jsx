ab694a97c5b81e0ac4ebb518f230f244bff3143e
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-18 07:23:25 +0500

    Update Layout.jsx

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -13,11 +13,11 @@ const Layout = () => {
     }
   },[isOwner])
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex min-h-screen flex-col'>
       <Navbar />
-      <div className='flex h-full'>
+      <div className='flex flex-1 overflow-hidden'>
         <Sidebar />
-        <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
+        <div className='flex-1 overflow-y-auto p-4 pt-8 md:px-10 md:pt-10'>
             <Outlet/>
         </div>
       </div>
