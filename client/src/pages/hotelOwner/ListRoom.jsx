66c6df41ce111ce1f9c240943913ebe9d69e0da4
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-18 07:23:37 +0500

    Update ListRoom.jsx

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -63,8 +63,8 @@ const ListRoom = () => {
         subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.'
       />
       <p className='text-gray-500 mt-8'>All Rooms</p>
-      <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll mt-3'>
-        <table className='w-full'>
+      <div className='w-full max-w-3xl overflow-x-auto text-left border border-gray-300 rounded-lg max-h-80 mt-3'>
+        <table className='w-full min-w-[620px]'>
           <thead className='bg-gray-50'>
             <tr>
               <th className='py-3 px-4 text-gray-800 font-medium'>Name</th>
