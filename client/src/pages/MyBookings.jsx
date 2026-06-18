e0dd86d45b30408827a47bd2c67249a9901680c9
Mujib ur Rahman <mujibkhan081@gmail.com>
2026-06-18 07:23:46 +0500

    Update MyBookings.jsx

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -47,7 +47,7 @@ const MyBookings = () => {
   },[user])
 
   return (
-    <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
+    <div className="py-28 md:pb-35 md:pt-32 px-4 sm:px-6 md:px-16 lg:px-24 xl:px-32">
       <Title
         title="My Bookings"
         subTitle="Easily manage your past, current, and upcoming hotel reservations in one place. Plan your trips seamlessly with just a few clicks"
@@ -55,7 +55,7 @@ const MyBookings = () => {
       />
 
       <div className="max-w-6xl mt-8 w-full text-gray-800">
-        <div className="hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
+        <div className="hidden lg:grid lg:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
           <div className="w-1/3">Hotels</div>
           <div className="w-1/3">Date & Timings</div>
           <div className="w-1/3">Payment</div>
@@ -64,17 +64,17 @@ const MyBookings = () => {
         {bookings.map((booking) => (
           <div
             key={booking._id}
-            className="grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 py-6 first:border-t"
+            className="grid grid-cols-1 gap-5 lg:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 py-6 first:border-t"
           >
             {/* ----- Hotel Details ----- */}
-            <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col sm:flex-row gap-4">
               <img
                 src={booking.room.images[0]}
                 alt="hotel-img"
-                className="min-md:w-44 rounded shadow object-cover"
+                className="h-52 w-full rounded shadow object-cover sm:h-36 sm:w-44"
               />
-              <div className="flex flex-col gap-1.5 max-md:mt-3 min-md:ml-4">
-                <p className="font-playfair text-2xl">
+              <div className="flex flex-col gap-1.5">
+                <p className="font-playfair text-xl sm:text-2xl">
                    {booking.hotel.name}
                   <span className="font-inter text-sm">  ({booking.room.roomType})
                   </span>
@@ -91,7 +91,7 @@ const MyBookings = () => {
               </div>
             </div>
             {/* ----- Date & Timeings ----- */}
-            <div className="flex flex-row md:items-center md:gap-12 mt-3 gap-8">
+            <div className="flex flex-row flex-wrap md:items-center md:gap-12 gap-8">
                 <div>
                     <p>Check-In</p>
                     <p className="text-gray-500 text-sm">
